92cd763e02310baa1d8106ab206c25015e760e83
leejun <wallmania1@daum.net>
2018-01-25 17:47:22 +0900

    My Django Girls app, second commit

--- kilogram/views.py
+++ kilogram/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 #from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.decorators import login_required
@@ -21,8 +22,20 @@ def upload(request):
     form = UploadForm()
     return render(request, 'kilogram/upload.html', {'form': form})
 
-class IndexView(TemplateView):
-    template_name = 'kilogram/index.html'
+
+# main page -> 내가 업로드한 사진을 보여주고 싶다
+# method -> 골뱅이 + login_required 할 수 있다
+# class -> 골뱅이 + login_required 할 수 없다
+class IndexView(ListView):
+    context_object_name = 'user_photo_list'
+    # template_name은 정해진 name을 쓰자 -> 발생하는 오류를 통해 알 수 있음
+    # template_name = 'kilogram/index.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.photo_set.all().order_by('-pub_date')
+
 
 class CreateUserView(CreateView):
     template_name = 'registration/signup.html'
